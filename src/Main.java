90bd177dad7ecddf42cb7ab0d2c15133f96bbe9c
lenysi4ka25 <lenysi4ka@mail.ru>
2022-08-24 17:19:48 +0300

    Есть!

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      var Boxer1 = 78.2;
+      var Boxer2 = 82.7;
+      var SummaPondusPugiles = Boxer1 + Boxer2;
+      System.out.println("Общий вес боксёров: " + SummaPondusPugiles + " кг.");
+      var DifferentiaInterPonderaPugiles = Boxer1 - Boxer2;
+        System.out.println("Разница между весами бойцов: " + DifferentiaInterPonderaPugiles + " кг.");
+    }
+}
